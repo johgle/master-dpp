65ad063880b350a291b68fcf6646a28be3449df8
johagl <johagl@stud.ntnu.no>
2025-06-13 15:43:54 +0200

    change foldername

--- DPP_project/config/urls.py
+++ DPP_project/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mittprosjekt project.
+URL configuration for the project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
